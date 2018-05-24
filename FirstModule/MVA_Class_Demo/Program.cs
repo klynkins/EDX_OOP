eda15e30e6b69bfeba2382464cf4bd02be5a0037
klynkins <klyncamp23@gmail.com>
2018-05-24 18:39:46 -0400

    "second commit"

---
+++ FirstModule/MVA_Class_Demo/Program.cs
@@ -0,0 +1,17 @@
+namespace MVA_Class_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinksMachine myMachine = new DrinksMachine("Kitchen", "Brand", "DM1000");
+            //myMachine.Location = "Kitchen";
+            //myMachine._model = "DM1000";
+
+            Console.WriteLine(myMachine.Location);
+            Console.WriteLine(myMachine.Make);
+            Console.WriteLine(myMachine.Model);
+            myMachine.MakeCappuccino();
+        }
+    }
+}
---
+++ FirstModule/StaticClass.cs
@@ -0,0 +1,7 @@
+static class Program
+{
+    static void Main(string[ args])
+    {
+        Console.WriteLine(Math.Pow(2, 8));
+    }
+}
